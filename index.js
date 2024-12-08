911231c4c3d6bab9080a7705802e9b9580add1ba
al-abir-anik <alabiranik56@gmail.com>
2024-12-08 20:53:36 +0600

    Delete a Favourite movie from database function added

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
       //   userEmail,
       //   _id: new ObjectId(movieId),
       // });
-  
+
       // if (existingFavorite) {
       //   return res
       //     .status(400)
@@ -67,6 +67,19 @@ async function run() {
 
       const favouriteMovie = { ...movie, userEmail };
       const result = await favouriteCollection.insertOne(favouriteMovie);
+      res.send(result);
+    });
+    // Get All Favourite Movie
+    app.get("/favourites", async (req, res) => {
+      const cursor = favouriteCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // For Delete Favourite movie
+    app.delete("/favourites/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await favouriteCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -89,7 +102,7 @@ async function run() {
       const result = await movieCollection.updateOne(filter, movie, options);
       res.send(result);
     });
-
+    // For Delete Movie
     app.delete("/movie/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
